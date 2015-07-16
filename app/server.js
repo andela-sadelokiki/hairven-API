f25cee3010e537c1087b5db5e22b8f7aa5d0eea0
andela-oogunwa <fadekemi.ogunwa@andela.co>
2015-07-16 15:46:33 +0100

    add user auth

--- app/server.js
+++ app/server.js
@@ -4,6 +4,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
+var passport = require('passport');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+
 var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
@@ -17,14 +22,28 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+require('./config/passport')(passport); // pass passport for configuration
+
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override'));
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'i' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set the static files location /public/img will be /img for users
 
 // routes ==================================================
+require('./app/routes/routes.js')(app, passport);
 
-// configure our routes
  app.use('/api', router);
 // start app ===============================================
 // startup our app at http://localhost:8080
